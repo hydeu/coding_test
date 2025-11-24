5b39cb64a55fb0f07d8270d1b8e74df287c01e86
plankjoo <mikkirestaurant@gmail.com>
2025-11-24 15:18:03 +0900

    [Bronze V] Title: A+B - 5, Time: 180 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		StringBuilder result = new StringBuilder();
+		
+		while(sc.hasNextInt()) {
+			
+			int A=sc.nextInt();
+			int B=sc.nextInt();
+			if (A == 0 && B == 0) break;
+			result.append(A+B).append("\n");
+			
+		}
+		
+		System.out.print(result);
+		sc.close();
+	}
+}
