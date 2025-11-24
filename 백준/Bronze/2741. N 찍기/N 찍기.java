af99275b7615874b24da8ddadb4a48682c2932e9
plankjoo <mikkirestaurant@gmail.com>
2025-11-24 11:51:33 +0900

    [Bronze V] Title: N 찍기, Time: 808 ms, Memory: 34068 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int jayeon = sc.nextInt();
+		
+		for(int i=1; i<=jayeon; i++) {
+			System.out.println(i);
+		}
+	}
+}
