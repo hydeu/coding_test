e1bce5aaf23811238fe10242a0cca3eceb77f9a6
plankjoo <mikkirestaurant@gmail.com>
2025-11-24 15:23:29 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char c = sc.nextLine().charAt(0);
+		int asc = (int)c;
+		
+		System.out.print(asc);
+	}
+}
